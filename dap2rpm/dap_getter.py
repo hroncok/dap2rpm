e2d3dc470a75fb83ebbcffa0932d4787a07d3bbd
Slavek Kabrda <bkabrda@redhat.com>
2014-11-12 10:46:41 +0100

    Allow getting DAPs from DAPI

--- dap2rpm/dap_getter.py
+++ dap2rpm/dap_getter.py
@@ -1,6 +1,9 @@
 import os
 import shutil
 
+import requests
+import yaml
+
 from dap2rpm import exceptions
 
 class DAPGetter(object):
@@ -33,7 +36,29 @@ class DAPGetter(object):
         Raises:
             exceptions.DAPGetException if getting the DAP fails
         """
-        pass
+        if self.version:
+            dap_url = '/'.join([self.dapi_api_url, 'daps',
+                '{0}-{1}'.format(self.dapname, self.version)])
+        else:
+            metadap_url = '/'.join([self.dapi_api_url, 'metadaps', self.dapname])
+            try:
+                dap_url = yaml.load(requests.get(metadap_url).text)['latest']
+            except KeyError:
+                raise exceptions.\
+                    DAPGetException('DAP "{0}" not found on DAPI'.format(self.dapname))
+        try:
+            download_url = yaml.load(requests.get(dap_url).text)['download']
+        except KeyError:
+            raise exceptions.DAPGetException('Can\'t get DAP "{0}", in version "{1}"'.
+                format(self.dapname, self.version))
+        resname = os.path.join(self.saveto, download_url.split('/')[-1])
+        download = requests.get(download_url, stream=True)
+        with open(resname, 'wb') as f:
+            for chunk in download.iter_content(chunk_size=2048):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+                    f.flush()
+        return resname
 
     def get_dap_local(self):
         """Copies dap from a local filesystem to self.saveto.
